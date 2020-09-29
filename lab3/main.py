23683b279e3aa287e97d8ce360e3d08f737fefe6
Biletskaya <bil.zlata@gmail.com>
2020-09-29 15:07:23 +0300

    pep8

--- lab3/main.py
+++ lab3/main.py
@@ -4,7 +4,19 @@ import pygame.draw as d
 pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
-d.circle(screen, (255, 255, 0), (100, 100), 50)
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray1 = (183, 200, 196)
+gray2 = (200, 200, 200)
+gray3 = (83, 108, 103)
+gray4 = (183, 196, 200)
+blue = (64, 128, 255)
+
+
+d.rect(screen, gray1, (0, 200, 500, 200))
+d.rect(screen, gray2, (100, 50, 40, 200))
+d.rect(screen, gray3, ())
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
